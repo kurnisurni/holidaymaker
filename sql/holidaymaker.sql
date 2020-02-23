92c8f586104978b05724a8f42b605d9c17aa7b9a
Kurnia <kurniasuryai@gmail.com>
2020-02-23 20:35:22 +0100

    sql

--- sql/holidaymaker.sql
+++ sql/holidaymaker.sql
@@ -11,13 +11,77 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for holidaymaker
+CREATE DATABASE IF NOT EXISTS `holidaymaker` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_swedish_ci */;
+USE `holidaymaker`;
+
+-- Dumping structure for table holidaymaker.accommodations
+CREATE TABLE IF NOT EXISTS `accommodations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(250) NOT NULL,
+  `country` varchar(50) NOT NULL,
+  `swimming_pool` bit(1) DEFAULT b'0',
+  `night_club` bit(1) DEFAULT b'0',
+  `restaurant` bit(1) DEFAULT b'0',
+  `kids_club` bit(1) DEFAULT b'0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table holidaymaker.bookings
+CREATE TABLE IF NOT EXISTS `bookings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `guest_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `room_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `number_of_guests` int(11) unsigned NOT NULL DEFAULT '0',
+  `check_in_date` date DEFAULT NULL,
+  `check_out_date` date DEFAULT NULL,
+  `total_price` double unsigned DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_bookings_guests` (`guest_id`),
+  KEY `FK_bookings_rooms` (`room_id`),
+  CONSTRAINT `FK_bookings_guests` FOREIGN KEY (`guest_id`) REFERENCES `guests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_bookings_rooms` FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
+
+-- Dumping structure for table holidaymaker.guests
+CREATE TABLE IF NOT EXISTS `guests` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table holidaymaker.rooms
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `room_type_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `accommodation_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `room_price` double unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `FK_rooms_room_types` (`room_type_id`),
+  KEY `FK_rooms_accommodations` (`accommodation_id`),
+  CONSTRAINT `FK_rooms_accommodations` FOREIGN KEY (`accommodation_id`) REFERENCES `accommodations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_rooms_room_types` FOREIGN KEY (`room_type_id`) REFERENCES `room_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table holidaymaker.room_types
+CREATE TABLE IF NOT EXISTS `room_types` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `room_type` enum('Single','Double','Family','VIP Suite') NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
